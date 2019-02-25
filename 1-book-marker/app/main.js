4a1d9f021a3dd0ca6f2457f16f1e7d304b086825
demon-monk <antdude@126.com>
2019-02-25 16:21:13 +0800

    feat: add render process (BrowserWindow)

--- 1-book-marker/app/main.js
+++ 1-book-marker/app/main.js
@@ -1,7 +1,9 @@
 const {
     app, // 1.hide, show, quit the application 2. events: before-quit, window-all-closed, browser-window-blur, and browser-window-focus
+    BrowserWindow, // 1. DOM 2. access chromium web api 3. Node built-in modules
 } = require('electron')
 
+let mainWindow = null
 app.on('ready', () => {
-    console.log('Hello from electron');
+    mainWindow = new BrowserWindow()
 })
