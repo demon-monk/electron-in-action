9a580ea1856c4f4181ca7543fdcae5cf3ce28276
demon-monk <antdude@126.com>
2019-02-25 19:18:45 +0800

    feat: add form element and fire request when submit the form

--- 1-book-marker/app/render.js
+++ 1-book-marker/app/render.js
@@ -1,4 +1,25 @@
-const button = document.querySelector('button')
-button.addEventListener('click', () => {
-    alert(__dirname) // alert is webAPI, __dirname is in node environment
-})
+const parser = new DOMParser();
+
+const linksSection = document.querySelector(".links");
+const errorMessage = document.querySelector(".error-message");
+const newLinkForm = document.querySelector(".new-link-form");
+const newLinkUrl = document.querySelector(".new-link-url");
+const newLinkSubmit = document.querySelector(".new-link-submit");
+const clearStorageButton = document.querySelector(".clear-storage");
+
+newLinkUrl.addEventListener("keyup", () => {
+  newLinkSubmit.disabled = !newLinkUrl.validity.valid;
+});
+const clearForm = () => {
+  newLinkUrl.value = null;
+};
+newLinkForm.addEventListener("submit", event => {
+  event.preventDefault();
+  const url = newLinkUrl.value;
+  fetch(url)
+    .then(res => res.text())
+    .then(parseResponseText)
+    .then(findTitle);
+});
+const parseResponseText = text => parser.parseFromString(text, "text/html");
+const findTitle = nodes => nodes.querySelector("title").innerText;
