19a190c601dfa9b1a814418741bbc8647c4db30b
demon-monk <antdude@126.com>
2019-02-25 16:30:24 +0800

    feat: add some content with HTML and load it into mainWindow

--- 1-book-marker/app/main.js
+++ 1-book-marker/app/main.js
@@ -2,8 +2,10 @@ const {
     app, // 1.hide, show, quit the application 2. events: before-quit, window-all-closed, browser-window-blur, and browser-window-focus
     BrowserWindow, // 1. DOM 2. access chromium web api 3. Node built-in modules
 } = require('electron')
+const path = require('path')
 
 let mainWindow = null
 app.on('ready', () => {
     mainWindow = new BrowserWindow()
+    mainWindow.webContents.loadFile(path.join(__dirname, 'index.html')) // should always use path.join
 })
