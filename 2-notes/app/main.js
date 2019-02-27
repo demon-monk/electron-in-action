e0432a57493e3ee9e7b13b47066bcbd528e11458
demon-monk <antdude@126.com>
2019-02-27 17:33:28 +0800

    feat: hide mainWindow before content is loaded

--- 2-notes/app/main.js
+++ 2-notes/app/main.js
@@ -3,8 +3,11 @@ const path = require('path')
 let mainWindow = null
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow()
+    mainWindow = new BrowserWindow({ show: false })
     mainWindow.loadFile(path.join(__dirname, 'index.html'))
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show()
+    })
     mainWindow.on('closed', () => {
         mainWindow = null
     })
