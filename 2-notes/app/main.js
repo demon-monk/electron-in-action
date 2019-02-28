08b5809f2871db2444b59f8f5f835ef461bd95dc
demon-monk <antdude@126.com>
2019-02-28 11:57:22 +0800

    create multiple windows and communicate between a certain render process and main process
    create multiple windows and communicate between a certain render process and main process

--- 2-notes/app/main.js
+++ 2-notes/app/main.js
@@ -2,20 +2,41 @@ const {app, BrowserWindow, dialog} = require('electron')
 const path = require('path')
 const fs = require('fs')
 let mainWindow = null
-
+const windows = new Set()
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({ show: false })
-    mainWindow.loadFile(path.join(__dirname, 'index.html'))
-    mainWindow.once('ready-to-show', () => {
-        mainWindow.show()
-    })
-    mainWindow.on('closed', () => {
-        mainWindow = null
-    })
+    createWindow()
 })
 
-const getFileFromUser = exports.getFileFromUser = () => {
-    const files = dialog.showOpenDialog({
+const createWindow = exports.createWindow = () => {
+    let newWindow = new BrowserWindow({ show: false })
+    newWindow.loadFile(path.join(__dirname, 'index.html'))
+    newWindow.once('ready-to-show', () => {
+        newWindow.show()
+    })
+    newWindow.on('closed', () => {
+        windows.delete(newWindow)
+        newWindow = null
+    })
+    windows.add(newWindow)
+    return newWindow
+}
+
+const createWindow = exports.createWindow = () => {
+    let newWindow = new BrowserWindow({ show: false })
+    newWindow.loadFile(path.join(__dirname, 'index.html'))
+    newWindow.once('ready-to-show', () => {
+        newWindow.show()
+    })
+    newWindow.on('closed', () => {
+        windows.delete(newWindow)
+        newWindow = null
+    })
+    windows.add(newWindow)
+    return newWindow
+}
+
+const getFileFromUser = exports.getFileFromUser = (targetWindow) => {
+    const files = dialog.showOpenDialog(targetWindow, {
         // other options: openDirectory, multiselections
         properties: ['openFile'],
         // restrict certain file types
@@ -25,11 +46,11 @@ const getFileFromUser = exports.getFileFromUser = () => {
         ],  
     })
     if (files && files[0]) {
-        openFile(files[0])
+        openFile(targetWindow, files[0])
     }
 }
 
-const openFile = (file) => {
+const openFile = exports.openFile = (targetWindow, file) => {
     const content = fs.readFileSync(file, 'utf8')
-    mainWindow.webContents.send('file-opened', file, content)
+    targetWindow.webContents.send('file-opened', file, content)
 }
