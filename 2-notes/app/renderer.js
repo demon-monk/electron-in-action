2fbe571261a420e2daa47506653c7782d74cb4db
demon-monk <antdude@126.com>
2019-03-02 12:25:36 +0800

    feat: add drop to open file feature

--- 2-notes/app/renderer.js
+++ 2-notes/app/renderer.js
@@ -37,6 +37,21 @@ const updateUserInterface = (isEdited) => {
     revertButton.disabled = !isEdited
 }
 
+// dragging, not dropped, can only get file metadata
+const getDraggedFile = event => {
+    return event.dataTransfer.items[0]
+}
+// dropped, can get real file data
+const getDroppedFile = event => event.dataTransfer.files[0]
+const fileTypeIsSupported = file => {
+    return ['text/plain', 'text/markdown'].includes(file.type)
+}
+const removeDropStyle = () => {
+    markdownView.classList.remove('drag-over')
+    markdownView.classList.remove('drag-error')
+}
+
+
 markdownView.addEventListener('keyup', (event) => {
     const currentContent = event.target.value
     renderMarkdownToHtml(currentContent)
@@ -62,6 +77,33 @@ saveMarkdownButton.addEventListener('click', () => {
 revertButton.addEventListener('click', () => {
     markdownView.value = originContent
     renderMarkdownToHtml(originContent)
+})
+
+document.addEventListener('dragstart', event => event.preventDefault())
+document.addEventListener('dragover', event => event.preventDefault())
+document.addEventListener('dragleave', event => event.preventDefault())
+document.addEventListener('drop', event => event.preventDefault())
+
+markdownView.addEventListener('dragover', event => {
+    const file = getDraggedFile(event)
+    if (fileTypeIsSupported(file)) {
+        console.log(file)
+        markdownView.classList.add('drag-over')
+    } else {
+        markdownView.classList.add('drag-error')
+    }
+})
+
+markdownView.addEventListener('dragleave', removeDropStyle)
+
+markdownView.addEventListener('drop', event => {
+    const file = getDroppedFile(event)
+    if (fileTypeIsSupported) {
+        mainProcess.openFile(currentWindow, file.path)
+    } else {
+        alert('This file type is not supported')
+    }
+    removeDropStyle()
 })
 
 ipcRenderer.on('file-opened', (event, file, content) => {
