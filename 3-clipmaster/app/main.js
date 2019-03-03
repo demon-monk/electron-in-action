1691cb91a6c0eb0a6976966120b18b1ebab224e8
demon-monk <antdude@126.com>
2019-03-04 00:42:02 +0800

    feat: get correct icon according to platform and dark mode

--- 3-clipmaster/app/main.js
+++ 3-clipmaster/app/main.js
@@ -1,12 +1,22 @@
 const path = require('path')
-const { app, Menu, Tray } = require('electron')
+const { app, Menu, Tray, systemPreferences } = require('electron')
 let tray = null
+
+const getIcon = () => {
+    if (process.platform === 'win32') {
+        return 'icon-light@2x.ico'
+    }
+    if (systemPreferences.isDarkMode()) {
+        return 'icon-light.png'
+    }
+    return 'icon-dark.png'
+}
 
 app.on('ready', () => {
     if (app.dock) {
         app.dock.hide()
     }
-    tray = new Tray(path.join(__dirname, './Icon.png'))
+    tray = new Tray(path.join(__dirname, getIcon()))
     if (process.platform === 'win32') {
         tray.on('click', tray.popUpContextMenu)
     }
