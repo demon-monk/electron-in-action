3fa3ea7fcea6b893119f4da6a7acdef21ef81acf
demon-monk <antdude@126.com>
2019-03-04 00:32:30 +0800

    feat: create tray instance and add a menu item for it

---
+++ 3-clipmaster/app/main.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const { app, Menu, Tray } = require('electron')
+let tray = null
+
+app.on('ready', () => {
+    if (app.dock) {
+        app.dock.hide()
+    }
+    tray = new Tray(path.join(__dirname, './Icon.png'))
+    if (process.platform === 'win32') {
+        tray.on('click', tray.popUpContextMenu)
+    }
+    const menu = Menu.buildFromTemplate([
+        {
+            label: 'Quit',
+            click () { app.quit() },
+        }
+    ])
+    tray.setToolTip('Clipmaster')
+    tray.setContextMenu(menu)
+})
